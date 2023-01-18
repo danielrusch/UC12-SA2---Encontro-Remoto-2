459da89491fb4b4486f73298c99ff1bc8097ad66
Daniel Rusch <daniel-rusch@hotmail.com>
2023-01-18 19:58:06 -0300

    encontro remoto 3

--- Program.cs
+++ Program.cs
@@ -3,12 +3,25 @@
 
 using Encontro_Remoto_2.Classes;
 
+Console.WriteLine($"PESSOA FÍSICA -----------------------------");
 PessoaFisica novaPf = new PessoaFisica();
 
 novaPf.nome = "Daniel";
 novaPf.cpf = "12365478965";
+novaPf.rendimento = 7000.51f;
 
-Console.WriteLine(novaPf.nome);
-Console.WriteLine(novaPf.cpf);
+float resultado = novaPf.CalcularImposto(novaPf.rendimento);
+Console.WriteLine(resultado);
+Console.WriteLine($"{resultado.ToString("C")}");
 
-Console.WriteLine($"Nome {novaPf.nome} CPF {novaPf.cpf}");
+// Console.WriteLine(novaPf.nome);
+// Console.WriteLine(novaPf.cpf);
+// Console.WriteLine($"Nome {novaPf.nome} CPF {novaPf.cpf}");
+
+Console.WriteLine($"PESSOA JURÍDICA -----------------------------");
+
+PessoaJuridica novaPj = new PessoaJuridica();
+
+float impostoPagar = novaPj.CalcularImposto(2000.5f);
+Console.WriteLine($"R$ {impostoPagar:0.00}");
+Console.WriteLine($"{impostoPagar.ToString("C")}");
